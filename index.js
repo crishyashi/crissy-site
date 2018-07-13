988a9ad52c13c6022307a847d53f09547222b34a
victor <victor@helium.com>
2018-07-13 15:03:00 -0700

    Add server routing

--- index.js
+++ index.js
@@ -1,13 +1,44 @@
 const http = require('http');
+const express = require('express');
+const app = express();
+const path = require('path');
 
 const PORT = process.env.PORT || 5000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+app.use(express.static('public'));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-server.listen(PORT, () => {
+app.get('/index.html', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.get('/codesign.html', function(req, res) {
+    res.sendFile(path.join(__dirname + '/codesign.html'));
+});
+
+app.get('/contact.html', function(req, res) {
+    res.sendFile(path.join(__dirname + '/contact.html'));
+});
+
+app.get('/prototyping.html', function(req, res) {
+    res.sendFile(path.join(__dirname + '/prototyping.html'));
+});
+
+app.get('/resume.html', function(req, res) {
+    res.sendFile(path.join(__dirname + '/resume.html'));
+});
+
+app.get('/usabilitytests.html', function(req, res) {
+    res.sendFile(path.join(__dirname + '/usabilitytests.html'));
+});
+
+app.get('/userinterviews.html', function(req, res) {
+    res.sendFile(path.join(__dirname + '/userinterviews.html'));
+});
+
+app.listen(PORT, () => {
   console.log(`Server running on ${PORT}/`);
 });
